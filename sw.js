69a700eebe0c46e700dabbef69e8c53f3dc47706
ptitom71960 <doin.thomas@gmail.com>
2026-06-24 21:38:45 +0200

    Update sw.js
    Tuto installation index ajouté

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker — Gardes & Astreintes (IADE / IBODE)
-const CACHE = 'gardes-astreintes-v2';
+const CACHE = 'gardes-astreintes-v3';
 const ASSETS = [
   './', './index.html',
   './gardes_astreintes_IADE.html', './gardes_astreintes_IBODE.html',
